8c9a3c07d6a2d421b265c5307d25ca08f00577c8
OrelNeto-DO <orelnetodo@gmail.com>
2025-02-02 05:19:56 +0200

    ...

--- terraform/auto_destroy.tf
+++ terraform/auto_destroy.tf
@@ -1,20 +1,23 @@
+# CloudWatch Event Rule for Destroy
 resource "aws_cloudwatch_event_rule" "auto_destroy" {
   name                = "auto-destroy-after-3h"
   description         = "Auto destroy all resources after 3 hours"
   schedule_expression = "rate(3 hours)"
 }
 
+# CloudWatch Event Target for Destroy
 resource "aws_cloudwatch_event_target" "auto_destroy" {
   rule      = aws_cloudwatch_event_rule.auto_destroy.name
   target_id = "AutoDestroyTarget"
   arn       = aws_lambda_function.auto_destroy.arn
 }
 
+# Lambda Function for Destroying Instances
 resource "aws_lambda_function" "auto_destroy" {
   filename      = "lambda/lambda_function.zip"
-  function_name = "auto_destroy"
+  function_name = "auto_destroy_resources"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
+  handler       = "destroy_handler.handler"
   runtime       = "nodejs16.x"
   timeout       = 900
 
@@ -22,5 +25,9 @@ resource "aws_lambda_function" "auto_destroy" {
     variables = {
       REGION = var.aws_region
     }
+  }
+
+  tags = {
+    Name = "AutoDestroy"
   }
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,6 +1,6 @@
 # IAM Role for Lambda
 resource "aws_iam_role" "lambda_role" {
-  name = "instance_stop_lambda_role"
+  name = "instance_management_lambda_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -18,7 +18,7 @@ resource "aws_iam_role" "lambda_role" {
 
 # IAM Policy for Lambda
 resource "aws_iam_role_policy" "lambda_policy" {
-  name = "instance_stop_lambda_policy"
+  name = "instance_management_lambda_policy"
   role = aws_iam_role.lambda_role.id
 
   policy = jsonencode({
@@ -28,7 +28,8 @@ resource "aws_iam_role_policy" "lambda_policy" {
         Effect = "Allow"
         Action = [
           "ec2:DescribeInstances",
-          "ec2:StopInstances"
+          "ec2:StopInstances",
+          "ec2:TerminateInstances"
         ]
         Resource = "*"
       },
@@ -45,12 +46,12 @@ resource "aws_iam_role_policy" "lambda_policy" {
   })
 }
 
-# Lambda Function
+# Lambda Function for Stopping Instances
 resource "aws_lambda_function" "stop_instances" {
-  filename         = "lambda_function.zip"
-  function_name    = "stop_instances"
+  filename         = "lambda/lambda_function.zip"
+  function_name    = "stop_instances_scheduled"
   role            = aws_iam_role.lambda_role.arn
-  handler         = "index.handler"
+  handler         = "stop_handler.handler"
   runtime         = "nodejs16.x"
   timeout         = 30
 
@@ -59,16 +60,20 @@ resource "aws_lambda_function" "stop_instances" {
       REGION = var.aws_region
     }
   }
+
+  tags = {
+    Name = "AutoStop"
+  }
 }
 
-# CloudWatch Event Rule
+# CloudWatch Event Rule for Stop
 resource "aws_cloudwatch_event_rule" "stop_instances" {
   name                = "stop-instances-evening"
-  description         = "Stop instances in the evening"
-  schedule_expression = "cron(0 20 * * ? *)"  # 20:00 UTC
+  description         = "Automatically stop instances every evening at 20:00 UTC"
+  schedule_expression = "cron(0 20 * * ? *)"
 }
 
-# CloudWatch Event Target
+# CloudWatch Event Target for Stop
 resource "aws_cloudwatch_event_target" "stop_instances" {
   rule      = aws_cloudwatch_event_rule.stop_instances.name
   target_id = "StopEC2Instances"
